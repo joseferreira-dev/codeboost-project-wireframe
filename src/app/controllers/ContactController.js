87c63648f6cf906f00463e644808c9ba85d40466
José Ferreira <joseguedes.dev@gmail.com>
2023-07-08 18:17:30 -0300

    Add ESLint config files and the contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // Listar todos os contatos
+  }
+
+  show() {
+    // Obter um registro
+  }
+
+  store() {
+    // Criar um registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+module.exports = new ContactController();
